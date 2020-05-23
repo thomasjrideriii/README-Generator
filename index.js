fcfe5165fd03647cf1e5c3bb0d6f8b8817a84c8a
Thomas Rider <thomasjrideriii@gmail.com>
2020-05-23 14:31:01 -0400

    Added file framework and imported README structure

---
+++ index.js
@@ -0,0 +1,61 @@
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+
+    `# Title
+
+    ![Licencing Icon](https://img.shields.io/static/v1?label=License&message=Licence&color=green)
+    
+    ## Description
+    
+    Placeholder Text!
+    
+    ## Table of Contents
+    
+    * [Installation](##Installation)
+    * [Usage](##Usage)
+    * [License](##License)
+    * [Contributing](##Contributing)
+    * [Tests](##Tests)
+    * [Questions](##Questions)
+    
+    ## Installation
+    
+    To install dependencies, please run the following code:
+    
+    
+        node i
+    
+    
+    ## Usage
+    
+    Placeholder Text!
+    
+    ## License
+    
+    This project is licensed under . . . . 
+    
+    ## Contributing
+    
+    Placeholder Text
+    
+    ## Tests
+    
+    To test, please run the following code:
+    
+    
+        node test
+    
+    
+    ## Questions
+    
+    For any questions or feedback, please contact [username](github.com/username) at <email@example.com>.`
+}
+
+function init() {
+
+}
+
+init();
